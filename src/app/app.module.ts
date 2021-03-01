fab593a5a21742eafa98890ea6ba5f047d4cd5c9
Jack Coyle <60261884+Jack-E-Coyle@users.noreply.github.com>
2021-03-01 22:50:55 +0000

    G C  movie-detail, editing .html .ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AddMovieComponent } from './add-movie/add-movie.component';
 import { ListMovieComponent } from './list-movie/list-movie.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddMovieComponent,
-    ListMovieComponent
+    ListMovieComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule
