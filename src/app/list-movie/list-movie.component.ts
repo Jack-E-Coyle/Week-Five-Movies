379c8706068ec09bcbc85defb6ddc8b0948616da
Jack Coyle <60261884+Jack-E-Coyle@users.noreply.github.com>
2021-03-01 22:39:45 +0000

    Changes to list-movie.component.ts and .html

--- src/app/list-movie/list-movie.component.ts
+++ src/app/list-movie/list-movie.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { MovieService} from '../movie.service';
+import { Movie } from "../movie.model";
 
 @Component({
   selector: 'app-list-movie',
@@ -9,8 +10,13 @@ import { MovieService} from '../movie.service';
 export class ListMovieComponent implements OnInit {
 
   movies=[];
+  private currentMovie:Movie;
 
-  constructor(private movieService: MovieService) { }
+  @Output() onSelectedMovie: EventEmitter<Movie>;
+
+  constructor(private movieService: MovieService) { 
+    this.onSelectedMovie=new EventEmitter();
+  }
 
   ngOnInit() {
     this.movies = this.movieService.getMovies();
